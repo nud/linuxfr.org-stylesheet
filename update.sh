6f387bcd651b30f491ffaad88bfbf30d80bbb6b7
Steve Frécinaux <code@istique.net>
2014-03-13 23:55:47 +0100

    Fix images.

--- update.sh
+++ update.sh
@@ -5,3 +5,9 @@
 
 ( cd ~/src/linuxfr.org && ./script/compile_sass app/assets/stylesheets/application.css.scss ) > $(dirname $0)/RonRonnement.css
 ( cd ~/src/linuxfr.org && ./script/compile_sass app/assets/stylesheets/contrib/FlexRonRon.css.scss ) > $(dirname $0)/FlexRonRon.css
+
+sed -i \
+	-e 's^url("/images^url("//linuxfr.org/images^' \
+	-e 's^url(/images^url(//linuxfr.org/images^' \
+	-e 's^url("/favicon^url("//linuxfr.org/favicon^' \
+	*.css
